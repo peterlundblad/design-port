b31bb7559df36427387191cd5ab02f112c19c768
Peter Lundblad <cfude88@gmail.com>
2020-05-12 16:36:37 -0700

    cta section is live on ascent page and changed some copy on about

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@ var gulp = require("gulp")
 var sass = require("gulp-sass")
 
 var defaultTask = function() {
-  // gulp + sass things here as usual
-}
+    }
+
+gulp.task("sass", function(){
+    return gulp.src("scss/input.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css/style.css"))
+})
+
+gulp.task("watch", function() {
+    gulp.watch("css/input.scss" ["sass"])
+})
 
 exports.default = defaultTask
